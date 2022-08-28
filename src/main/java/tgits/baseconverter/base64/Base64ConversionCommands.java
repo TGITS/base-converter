6a4cc64be442533a578237c3dd3d9fcc160e7ea1
TGITS <thegeekintheshell@gmail.com>
2022-08-28 18:02:25 +0200

    feat(spring-project-init)

--- src/main/java/tgits/baseconverter/base64/Base64ConversionCommands.java
+++ src/main/java/tgits/baseconverter/base64/Base64ConversionCommands.java
@@ -9,14 +9,19 @@ import java.util.Base64;
 @ShellComponent
 public class Base64ConversionCommands {
 
+    private Base64.Encoder encoder;
+    private Base64.Decoder decoder;
+    private Charset selectedCharset = Charset.defaultCharset();
+
     @ShellMethod(value = "Encode the provided string to Base 64.", key = {"to-base64", "toBase64", "to-b64", "toB64"})
     public String toBase64(String toEncodeString) {
-        return Base64.getEncoder().encodeToString(toEncodeString.getBytes(Charset.defaultCharset()));
+        byte[] encodedBytesArray = Base64.getEncoder().encode(toEncodeString.getBytes(selectedCharset));
+        return new String(encodedBytesArray,selectedCharset);
     }
 
     @ShellMethod(value = "Decode from Base 64 the provided string.", key = {"from-base64", "from-Base64", "from-b64", "fromB64"})
     public String fromBase64(String encodedString) {
         byte[] decodedBytes = Base64.getDecoder().decode(encodedString);
-        return new String(decodedBytes, Charset.defaultCharset());
+        return new String(decodedBytes, selectedCharset);
     }
 }
